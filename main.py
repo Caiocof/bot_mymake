ab46de9d85a3a11b67e7669ec9ecfe4264e1e25b
caiocof <caiooliveira3652@outlook.com>
2024-09-09 18:34:06 -0300

    refact: change method to start api

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
+import os
+import uvicorn
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 from start_boot import start_script
 
 app = FastAPI()
 
-origins = ["*"]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 start_script()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    return {"message": "Hello, Render!"}
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
