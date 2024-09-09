e5907241f7ae6a5ba9416ef7baef0d9d79adbcdb
caiocof <caiooliveira3652@outlook.com>
2024-09-09 15:20:08 -0300

    refact: add API to deploy

--- main.py
+++ main.py
@@ -1,42 +1,22 @@
-import os
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from bot_config import BOT
-from controller.product import get_product_name
-from controller.receivable import get_receivable
-from controller.reports import get_current_storage, get_sale_per_due_date
-from controller.sale import sele_product
-from utils.buttons import get_menu_keyboard, get_back_to_menu_keyboard
+from start_boot import start_script
 
+app = FastAPI()
 
-@BOT.message_handler(commands=['start'])
-def send_welcome(message):
-    BOT.reply_to(message, "Bem-vindo ao sistema de estoque!\n /menu para ver as opções do sistema")
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-
-@BOT.message_handler(commands=['menu'])
-def show_menu(message):
-    markup = get_menu_keyboard()
-    BOT.send_message(message.from_user.id, "Escolha o que deseja fazer!", reply_markup=markup)
+start_script()
 
 
-@BOT.callback_query_handler(func=lambda call: True)
-def callback_handler(call):
-    menu_button = get_back_to_menu_keyboard()
-    if call.data == "menu":
-        show_menu(call)
-    elif call.data == "new_product":
-        get_product_name(call, menu_button)
-    elif call.data == "new_sale":
-        sele_product(call, menu_button)
-    elif call.data == "get_storage":
-        get_current_storage(call, menu_button)
-    elif call.data == "seal_for_day":
-        get_sale_per_due_date(call, menu_button)
-    elif call.data == "receivable_accounts":
-        get_receivable(call, menu_button)
-
-
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', '8080'))
-    print(f"Start bot in port {PORT}")
-    BOT.infinity_polling()
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
---
+++ start_boot.py
@@ -0,0 +1,41 @@
+import os
+
+from bot_config import BOT
+from controller.product import get_product_name
+from controller.receivable import get_receivable
+from controller.reports import get_current_storage, get_sale_per_due_date
+from controller.sale import sele_product
+from utils.buttons import get_menu_keyboard, get_back_to_menu_keyboard
+
+
+@BOT.message_handler(commands=['start'])
+def send_welcome(message):
+    BOT.reply_to(message, "Bem-vindo ao sistema de estoque!\n /menu para ver as opções do sistema")
+
+
+@BOT.message_handler(commands=['menu'])
+def show_menu(message):
+    markup = get_menu_keyboard()
+    BOT.send_message(message.from_user.id, "Escolha o que deseja fazer!", reply_markup=markup)
+
+
+@BOT.callback_query_handler(func=lambda call: True)
+def callback_handler(call):
+    menu_button = get_back_to_menu_keyboard()
+    if call.data == "menu":
+        show_menu(call)
+    elif call.data == "new_product":
+        get_product_name(call, menu_button)
+    elif call.data == "new_sale":
+        sele_product(call, menu_button)
+    elif call.data == "get_storage":
+        get_current_storage(call, menu_button)
+    elif call.data == "seal_for_day":
+        get_sale_per_due_date(call, menu_button)
+    elif call.data == "receivable_accounts":
+        get_receivable(call, menu_button)
+
+
+def start_script():
+    print(f"Start bot")
+    BOT.infinity_polling()
