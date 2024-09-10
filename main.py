38967ce8c75a9d274d869f5e1eb9dcd74e5f25a0
caiocof <caiooliveira3652@outlook.com>
2024-09-10 12:21:41 -0300

    refact: te last time

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-from typing import Optional
-
+import os
+import threading
+import uvicorn
 from fastapi import FastAPI
+import telebot
 
 from start_boot import start_script
 
 app = FastAPI()
 
-start_script()
-
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    return {"message": "Hello from FastAPI"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+def start_fastapi():
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+
+
+if __name__ == "__main__":
+    # Inicia o FastAPI e o bot do Telegram em threads separadas
+    fastapi_thread = threading.Thread(target=start_fastapi)
+    bot_thread = threading.Thread(target=start_script)
+
+    fastapi_thread.start()
+    bot_thread.start()
+
+    fastapi_thread.join()
+    bot_thread.join()
